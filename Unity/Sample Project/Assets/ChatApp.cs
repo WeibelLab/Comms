052e450f076af44de82fabbf124e72cf49dbd6f6
Tommy Sharkey <tommy.l.sharkey@gmail.com>
2020-05-20 18:09:44 -0700

    Unity Sample Project - allowing dynamic communication changes

--- Unity/Sample Project/Assets/ChatApp.cs
+++ Unity/Sample Project/Assets/ChatApp.cs
@@ -16,7 +16,7 @@ public class ChatApp : MonoBehaviour
 
     public void ReadResponse(string message)
     {
-        Output.text += "\n"+this.Comm.Host.Name+" > " + message;
+        Output.text += "\n" + this.Comm.Host.Name + " > " + message;
     }
 
     public void OnConnect(ReliableCommunication Client)
@@ -31,18 +31,30 @@ public class ChatApp : MonoBehaviour
 
     public void ChangePort(string port)
     {
-        this.Comm.Host = new CommunicationEndpoint(this.Comm.Host.Address, int.Parse(port), "My Friend");
+        int Port = int.Parse(port);
+        this.Comm.Host.SetPort(Port); // if client
+        this.Comm.ListenPort = Port; // if server
         this.Comm.ForceReconnect();
+        Output.text += string.Format("\nService Modification: {0}",
+            (this.Comm.isServer) ? string.Format("Hosting on {0}", this.Comm.ListenPort) : string.Format("Connecting to {0}:{1}", this.Comm.Host.Address, this.Comm.Host.Port)
+            );
     }
 
-    public void ChangeAddress(string address) {
-        this.Comm.Host = new CommunicationEndpoint(address, this.Comm.ListenPort, "My Friend");
+    public void ChangeAddress(string address)
+    {
+        this.Comm.Host.SetAddress(address);
         this.Comm.ForceReconnect();
+        Output.text += string.Format("\nService Modification: {0}",
+            (this.Comm.isServer) ? string.Format("Hosting on {0}", this.Comm.ListenPort) : string.Format("Connecting to {0}:{1}", this.Comm.Host.Address, this.Comm.Host.Port)
+            );
     }
 
     public void ToggleServer(bool isClient)
     {
         this.Comm.isServer = !isClient;
         this.Comm.ForceReconnect();
+        Output.text += string.Format("\nService Modification: {0}",
+            (this.Comm.isServer) ? string.Format("Hosting on {0}", this.Comm.ListenPort) : string.Format("Connecting to {0}:{1}", this.Comm.Host.Address, this.Comm.Host.Port)
+            );
     }
 }
